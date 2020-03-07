4011d574c7e018ebd717116aefb663585423cd7b
Alexey <alexindilex@yandex.ru>
2020-03-07 20:07:28 +0300

    создали json

--- src/classes/Test.js
+++ src/classes/Test.js
@@ -1,4 +1,24 @@
-export default class Test {
+import Service from "./Service";
+import { Task } from "./Task";
 
+export default class Test {
+    constructor() {
+        this._service = new Service("./data.json");
+        this._service.getTask().then(data => this._createTasks(data));
+        this._tasks = [];
+        this._current = -1;
+    }
+
+    _createTasks() {
+        this._tasks = data.map(Task.create);
+        this._current = 0;
+    }
+
+    get task() {
+        if (this._current >= 0) {
+            return this._tasks[this._current];
+        }
+        return null;
+    }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import Task from "./classes/Task";
 import Test from "./classes/Test";
-import Service from "./classes/Service";
+
 
 
 window.addEventListener("load", () => {
-   const app = new Test();
+   window.test = new Test();
     });
 
 
