837b89b1e4ad6cacc33c8a827e13db8cad1ec90c
Alexey <alexindilex@yandex.ru>
2020-03-07 17:47:30 +0300

    Добавлены заготовки для основных классов

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Task from "./classes/Task";
+import Test from "./classes/Test";
+import Service from "./classes/Service";
+
+
+window.addEventListener("load", () => {
+   const app = new Test();
+});
+
